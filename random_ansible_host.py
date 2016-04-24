2da0025fe7216fd07ec374062cec4708b98fd6c4
Simon Fondrie-Teitler <simonft@riseup.net>
2016-04-24 17:33:45 -0400

    Add a docstring to cli method describing what the program does

--- random_ansible_host.py
+++ random_ansible_host.py
@@ -18,6 +18,8 @@ import subprocess
 @click.option('--vault-password-command', default='', help="command to run to get vault password")
 @click.pass_context
 def cli(ctx, inventory, vault_password_command):
+    """SSH into a random host in an ansible group"""
+
     ctx.obj = {'inventory': inventory,
                'vault_password_command': vault_password_command,
                }
